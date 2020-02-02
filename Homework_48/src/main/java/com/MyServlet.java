82576d211b5d21a42eb563b0daadaf4b6e469157
Bekbolot <nurmanbetovbekbolot@gmail.com>
2020-02-03 01:18:55 +0600

    hw

---
+++ Homework_48/src/main/java/com/MyServlet.java
@@ -0,0 +1,16 @@
+package com;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MyServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //super.doGet(req, resp);
+//        req.setAttribute("name","JAVA");
+        req.getRequestDispatcher("new.jsp").forward(req,resp);
+    }
+}
